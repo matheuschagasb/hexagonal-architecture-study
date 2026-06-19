fb797f67e5f3b1f7073045e1972b6a996ab22c12
Matheus Chagas <matheusssbbb539@gmail.com>
2026-06-19 18:12:06 -0300

    feat: implement input and output ports

---
+++ src/main/java/br/com/mcb/hexagonal_architecture_study/application/port/input/BuscarDadosUseCase.java
@@ -0,0 +1,8 @@
+package br.com.mcb.hexagonal_architecture_study.application.port.input;
+
+import java.util.List;
+
+public interface BuscarDadosUseCase {
+
+    List<Object> buscarDados();
+}
---
+++ src/main/java/br/com/mcb/hexagonal_architecture_study/application/port/output/BuscarDadosOutputPort.java
@@ -0,0 +1,8 @@
+package br.com.mcb.hexagonal_architecture_study.application.port.output;
+
+import java.util.List;
+
+public interface BuscarDadosOutputPort {
+
+    List<Object> buscarDados();
+}
