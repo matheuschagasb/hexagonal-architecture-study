8e8ecfa8e38dc89ea11d3b0d2cb1e40a21fba09a
Matheus Chagas <matheusssbbb539@gmail.com>
2026-06-19 16:38:40 -0300

    first commit

---
+++ src/main/java/br/com/mcb/hexagonal_architecture_study/HexagonalArchitectureStudyApplication.java
@@ -0,0 +1,13 @@
+package br.com.mcb.hexagonal_architecture_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchitectureStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mcb/hexagonal_architecture_study/HexagonalArchitectureStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mcb.hexagonal_architecture_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
